f7a476c8caa6ee5e82ec355c3cdf9929e7e0a963
Cyan11 <guilhermelacerda620@gmail.com>
2021-09-02 23:05:46 -0300

    Add project files.

---
+++ Experiments in DNA Computation.cpp
@@ -0,0 +1,20 @@
+// Experiments in DNA Computation.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+class Nucleotide
+{
+public:
+	char Type;
+	void Set(char type) {
+	switch (type) {
+	case 'A'|'a':
+	}
+}
+};
+class Chromosome
+{
+	Nucleotide One;
+	Nucleotide Two;
+};
